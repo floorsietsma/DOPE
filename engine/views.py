d2eac0f7bf66fc22b24e970e181625e617e81f61
Floor Sietsma <floor.sietsma@nike.com>
2023-04-06 16:05:15 +0200

    Using mixins

--- engine/views.py
+++ engine/views.py
@@ -1,52 +1,42 @@
 from rest_framework import status
 from rest_framework.response import Response
 from django.http import Http404
+from rest_framework import mixins
+from rest_framework import generics
 from engine.models import Machine
 from engine.serializers import MachineSerializer
 
 
-class MachineList(APIView):
+class MachineList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
     """
     List all machines, or create a new machine.
     """
+    queryset = Machine.objects.all()
+    serializer_class = MachineSerializer
 
-    def get(self, request, format=None):
-        machines = Machine.objects.all()
-        serializer = MachineSerializer(machines, many=True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = MachineSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-
-class MachineDetail(APIView):
+class MachineDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
     """
     Retrieve, update or delete a machine.
     """
-    def get_object(self, pk):
-        try:
-            return Machine.objects.get(pk=pk)
-        except Machine.DoesNotExist:
-            raise Http404
+    queryset = Machine.objects.all()
+    serializer_class = MachineSerializer
 
-    def get(self, request, pk, format=None):
-        machine = self.get_object(pk)
-        serializer = MachineSerializer(machine)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        machine = self.get_object(pk)
-        serializer = MachineSerializer(machine, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        machine = self.get_object(pk)
-        machine.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
